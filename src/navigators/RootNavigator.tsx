60f8cf2c8dd56a1ab3ad5417cfacff6fba8d491a
Marty Sun <martysun25@gmail.com>
2018-12-09 23:12:16 +0800

    remove Guest screen and only use anonymous signin

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { createSwitchNavigator, createAppContainer } from "react-navigation";
 
-import GuestNavigator from "./GuestNavigator";
 import MainNavigator from "./MainNavigator";
+import Landing from "../screens/Landing";
 
 const RootNavigator = createSwitchNavigator({
-  Guest: GuestNavigator,
+  Landing,
   Main: MainNavigator,
 });
 
---
+++ src/screens/Landing.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { NavigationScreenProp } from "react-navigation";
+import firebase from "react-native-firebase";
+
+interface Props {
+  navigation: NavigationScreenProp<any, any>;
+}
+
+export class Landing extends Component<Props> {
+  public async componentDidMount() {
+    try {
+      await firebase.auth().signInAnonymously();
+
+      this.props.navigation.navigate("Main");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  public render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text> Loading ... </Text>
+      </View>
+    );
+  }
+}
+
+export default Landing;
