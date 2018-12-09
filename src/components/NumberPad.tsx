33fa3383201e912f07b6b32e7acc804b73da2654
Marty Sun <martysun25@gmail.com>
2018-12-10 04:29:22 +0800

    number pad

---
+++ src/components/NumberPad.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Button, Text, View, Icon } from "native-base";
+import _ from "lodash";
+
+import color from "../theme/color";
+
+const styles: any = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  button: {
+    margin: 6,
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: color.secondary,
+  },
+  buttonContent: {
+    color: color.light,
+    fontSize: 30,
+  },
+});
+
+interface Props {
+  dot?: boolean;
+  onButtonPressed: (value: number | string) => void;
+}
+
+export default (props: Props) => (
+  <View>
+    <View style={styles.row}>
+      <Button rounded style={[styles.button, { marginLeft: 0 }]} onPress={() => props.onButtonPressed(7)}>
+        <Text style={styles.buttonContent}>7</Text>
+      </Button>
+      <Button rounded style={styles.button} onPress={() => props.onButtonPressed(8)}>
+        <Text style={styles.buttonContent}>8</Text>
+      </Button>
+      <Button rounded style={[styles.button, { marginRight: 0 }]} onPress={() => props.onButtonPressed(9)}>
+        <Text style={styles.buttonContent}>9</Text>
+      </Button>
+    </View>
+    <View style={styles.row}>
+      <Button rounded style={[styles.button, { marginLeft: 0 }]} onPress={() => props.onButtonPressed(4)}>
+        <Text style={styles.buttonContent}>4</Text>
+      </Button>
+      <Button rounded style={styles.button} onPress={() => props.onButtonPressed(5)}>
+        <Text style={styles.buttonContent}>5</Text>
+      </Button>
+      <Button rounded style={[styles.button, { marginRight: 0 }]} onPress={() => props.onButtonPressed(6)}>
+        <Text style={styles.buttonContent}>6</Text>
+      </Button>
+    </View>
+    <View style={styles.row}>
+      <Button rounded style={[styles.button, { marginLeft: 0 }]} onPress={() => props.onButtonPressed(1)}>
+        <Text style={styles.buttonContent}>1</Text>
+      </Button>
+      <Button rounded style={styles.button} onPress={() => props.onButtonPressed(2)}>
+        <Text style={styles.buttonContent}>2</Text>
+      </Button>
+      <Button rounded style={[styles.button, { marginRight: 0 }]} onPress={() => props.onButtonPressed(3)}>
+        <Text style={styles.buttonContent}>3</Text>
+      </Button>
+    </View>
+    <View style={styles.row}>
+      <Button rounded style={[styles.button, { flex: 3, marginLeft: 0 }]} onPress={() => props.onButtonPressed(0)}>
+        <Text style={styles.buttonContent}>0</Text>
+      </Button>
+      {props.dot && (
+        <Button rounded style={styles.button} onPress={() => props.onButtonPressed("dot")}>
+          <Text style={styles.buttonContent}>．</Text>
+        </Button>
+      )}
+      <Button
+        rounded
+        style={[styles.button, { flex: 2, marginRight: 0 }]}
+        onPress={() => props.onButtonPressed("backspace")}
+      >
+        <Icon type="MaterialIcons" name="arrow-back" style={styles.buttonContent} />
+      </Button>
+    </View>
+  </View>
+);
--- src/screens/add-spending/AddSpending.tsx
+++ src/screens/add-spending/AddSpending.tsx
@@ -1,13 +1,59 @@
 import React, { Component } from "react";
-import { Content, Text } from "native-base";
+import { Content, Text, View, Button, Icon } from "native-base";
 import { SafeAreaView } from "react-native";
+import _ from "lodash";
+
+import NumberPad from "../../components/NumberPad";
+import color from "../../theme/color";
 
 export class AddSpending extends Component {
+  public state = {
+    spending: 0,
+  };
+
+  public handleNumberPadPressed = (value: number | string) => {
+    if (_.isNumber(value) && this.state.spending <= 10e10) {
+      this.setState({ spending: this.state.spending * 10 + value });
+    }
+
+    if (value === "backspace") {
+      return this.setState({ spending: _.floor(this.state.spending / 10) });
+    }
+  };
+
   public render() {
     return (
       <SafeAreaView style={{ flex: 1 }}>
-        <Content>
-          <Text>Add Spending</Text>
+        <Content scrollEnabled={false} contentContainerStyle={{ flex: 1, justifyContent: "flex-end" }}>
+          <View style={{ marginHorizontal: 15, marginVertical: 10 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                borderWidth: 5,
+                borderColor: color.primary,
+                borderRadius: 20,
+                marginBottom: 5,
+              }}
+            >
+              <Text numberOfLines={1} style={{ flex: 4, fontSize: 24, padding: 10, paddingLeft: 15 }}>
+                {this.state.spending}
+              </Text>
+              <Button
+                activeOpacity={1}
+                style={{
+                  flex: 1,
+                  height: "100%",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  borderRadius: 0,
+                  backgroundColor: color.primary,
+                }}
+              >
+                <Icon type="Feather" name="check" style={{ color: color.light }} />
+              </Button>
+            </View>
+            <NumberPad onButtonPressed={this.handleNumberPadPressed} />
+          </View>
         </Content>
       </SafeAreaView>
     );
