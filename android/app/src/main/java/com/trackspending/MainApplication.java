b6611ee11a2a12df8ba6cbe6a0ce8bc35d403816
Marty Sun <martysun25@gmail.com>
2018-12-12 00:15:50 +0800

    [native-lib][react-native-firebase] install firestore on iOS/Android

--- android/app/src/main/java/com/trackspending/MainApplication.java
+++ android/app/src/main/java/com/trackspending/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebaseFirestorePackage(),
             new RNFirebaseAuthPackage(),
             new RNFirebasePackage(),
             new RNGestureHandlerPackage()
