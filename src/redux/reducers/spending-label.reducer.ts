eff75f883ab55c12086ce2d117b3f049f72a4f59
Marty Sun <martysun25@gmail.com>
2018-12-17 02:04:45 +0800

    update, delete method on spending-label reducer

--- src/redux/reducers/spending-label.reducer.ts
+++ src/redux/reducers/spending-label.reducer.ts
@@ -3,13 +3,19 @@ import { produce } from "immer";
 import { normalize, schema } from "normalizr";
 import _ from "lodash";
 
-import { AppState, SpendingLabel } from "../../typings";
+import { AppState, SpendingLabel, Category } from "../../typings";
 import { getDefaultSpendingLabels } from "../../utils";
 
 const CREATE_DEFAULT_SPENDING_LABELS = "SPENDING-LABEL:CREATE_DEFAULT_SPENDING_LABELS";
+const CREATE_SPENDING_LABEL = "SPENDING-LABEL:CREATE_SPENDING_LABEL";
+const UPDATE_SPENDING_LABEL = "SPENDING-LABEL:UPDATE_SPENDING_LABEL";
+const DELETE_SPENDING_LABEL = "SPENDING-LABEL:DELETE_SPENDING_LABEL";
 
 export const spendingLabelActionTypes = {
   CREATE_DEFAULT_SPENDING_LABELS,
+  CREATE_SPENDING_LABEL,
+  UPDATE_SPENDING_LABEL,
+  DELETE_SPENDING_LABEL,
 };
 
 const createDefaultSpendingLabels = (): any => ({
@@ -17,8 +23,26 @@ const createDefaultSpendingLabels = (): any => ({
   payload: { normalizedSpendingLabels: normalize(getDefaultSpendingLabels(), [new schema.Entity("spendingLabels")]) },
 });
 
+const createSpendingLabel = (spendingLabel: SpendingLabel) => ({
+  type: CREATE_SPENDING_LABEL,
+  payload: { spendingLabel },
+});
+
+const updateSpendingLabel = (id: string, properties: { category?: Category; name?: string }) => ({
+  type: UPDATE_SPENDING_LABEL,
+  payload: { id, properties },
+});
+
+const deleteSpendingLabel = (id: string) => ({
+  type: DELETE_SPENDING_LABEL,
+  payload: { id },
+});
+
 export const spendingLabelActionCreators = {
   createDefaultSpendingLabels,
+  createSpendingLabel,
+  updateSpendingLabel,
+  deleteSpendingLabel,
 };
 
 const ids = (state: string[] = [], { type, payload }: { type: string; payload: any }) =>
@@ -26,6 +50,11 @@ const ids = (state: string[] = [], { type, payload }: { type: string; payload: a
     switch (type) {
       case CREATE_DEFAULT_SPENDING_LABELS:
         return payload.normalizedSpendingLabels.result;
+      case CREATE_SPENDING_LABEL:
+        draft.push(payload.spendingLabel.id);
+        break;
+      case DELETE_SPENDING_LABEL:
+        return _.filter(state, id => id !== payload.id);
     }
   });
 
@@ -34,6 +63,15 @@ const byId = (state: { [id: string]: SpendingLabel } = {}, { type, payload }: { 
     switch (type) {
       case CREATE_DEFAULT_SPENDING_LABELS:
         return payload.normalizedSpendingLabels.entities.spendingLabels;
+      case CREATE_SPENDING_LABEL:
+        draft[payload.spendingLabel.id] = payload.spendingLabel;
+        break;
+      case UPDATE_SPENDING_LABEL:
+        draft[payload.id] = { ...state[payload.id], ...payload.properties };
+        break;
+      case DELETE_SPENDING_LABEL:
+        delete draft[payload.id];
+        break;
     }
   });
 
@@ -43,5 +81,9 @@ export default combineReducers({
 });
 
 export const spendingLabelSelectors = {
-  getSpendingLabels: (state: AppState) => state.spendingLabel.ids.map(id => state.spendingLabel.byId[id]),
+  getSpendingLabels: (state: AppState) =>
+    _.chain(state.spendingLabel.ids)
+      .map(id => state.spendingLabel.byId[id])
+      .orderBy("category")
+      .value(),
 };
